ab5714f5d95244cf907e016b23b5280836c60797
Osama <osamashatat99@gmail.com>
2023-01-15 23:10:09 +0200

    Update from Ahmad

--- Main.java
+++ Main.java
@@ -3,4 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello My name is Osama");
 
+        System.out.println("Hello My name is Ahmad");
+
     }}
